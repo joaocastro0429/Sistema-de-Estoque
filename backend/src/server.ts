2cd3f9d26b01be7b633b43ce2b9218cd022998b7
joaocastro <joaocastro0429@gmail.com>
2025-05-31 10:12:50 -0300

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,7 +8,12 @@ import cors from 'cors'
 const server=express()
 
 server.use(express.json())
-server.use(cors())
+server.use(cors(
+    {
+            origin:
+            'https://sistema-estoque-frontend.onrender.com'   
+    }
+))
 
 server.use(router)
 server.use(UserRouter)
